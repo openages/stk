dadc0dfee665226cc2594e53b9d7ab61f9fca15d
Wendao <xiewendao123@foxmail.com>
2024-02-13 15:33:42 +0800

    remove(mobx): transaction by mobx default action behavior

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -1,4 +1,3 @@
 export { default as setStorageWhenChange } from './setStorageWhenChange'
 export { default as useInstanceWatch } from './useInstanceWatch'
-export { default as transaction } from './transaction'
 export { Watch } from './types'
